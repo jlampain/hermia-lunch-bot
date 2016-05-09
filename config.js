2b8feb7d4fa5a93c9e3f05a228b55fa4418a60a4
Jussi Lampainen <jussi.lampainen@tecnotree.com>
2016-05-09 14:46:40 +0300

    config changes for travic ci

--- config.js
+++ config.js
@@ -1,14 +1,15 @@
 'use strict';
 
-const dotenv = require('dotenv');
-const ENV = process.env.NODE_ENV || 'development';
+const dotenv = require('dotenv'),
+	  ENV = process.env.NODE_ENV || 'development',
+	  fs = require('fs');
 
-if (ENV === 'development') dotenv.load();
+if (ENV === 'development' && fs.existsSync('.env')) dotenv.load();
 
 const config = {
-    PORT: process.env.PORT,
-    LUNCHBOT_WEBHOOK_URL: process.env.LUNCHBOT_WEBHOOK_URL,
-    LUNCHBOT_COMMAND_TOKEN: process.env.LUNCHBOT_COMMAND_TOKEN,
+    PORT: process.env.PORT || 3000,
+    LUNCHBOT_WEBHOOK_URL: process.env.LUNCHBOT_WEBHOOK_URL || '',
+    LUNCHBOT_COMMAND_TOKEN: process.env.LUNCHBOT_COMMAND_TOKEN || '',
     ICON_EMOJI: ':fork_and_knife:',
     SLACK_CHANNEL: '#lunch-match-maker'
 };
